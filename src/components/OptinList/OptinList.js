94447327a03d64f8f6c6a1b0b2c1a29e1f82e6b2
Yididiya <yidisam18@gmail.com>
2022-06-19 16:57:08 +0300

    optin list accept/decline

--- src/components/OptinList/OptinList.js
+++ src/components/OptinList/OptinList.js
@@ -1,6 +1,51 @@
 import { Button } from 'reactstrap';
 
 function OptinList({ traineesOptin }) {
+  const handleAccept = ({ name, address, asset_id }) => {
+    fetch('http://localhost:8000/api/v1/nft/transfer', {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        address,
+        asset_id,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.status === 'fail') {
+          console.log(res);
+          alert(res.message);
+        } else {
+          alert('Request Accepted!');
+        }
+      });
+  };
+
+  const handleDecline = ({ name, address }) => {
+    console.log(name, address);
+    // http://localhost:8000/api/v1/nft
+    fetch('http://localhost:8000/api/v1/trainees/optin', {
+      // Adding method type
+      method: 'DELETE',
+
+      // Adding body or contents to send
+      body: JSON.stringify({
+        name,
+        address,
+      }),
+
+      // Adding headers to the request
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      // Converting to JSON
+      .then((response) => response.json())
+      .then(console.log);
+  };
   return traineesOptin.map((trainee) => {
     return (
       <tr key={trainee._id}>
@@ -8,10 +53,18 @@ function OptinList({ traineesOptin }) {
         <td>{trainee.address}</td>
         {/* <td>{trainee.status}</td> */}
         <td className="text-center">
-          <Button variant="success" className="btn-success">
+          <Button
+            variant="success"
+            className="btn-success"
+            onClick={() => handleAccept(trainee)}
+          >
             Accept
           </Button>{' '}
-          <Button variant="danger" className="btn-danger">
+          <Button
+            variant="danger"
+            className="btn-danger"
+            onClick={() => handleDecline(trainee)}
+          >
             Decline
           </Button>
         </td>
